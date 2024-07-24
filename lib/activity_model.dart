48c8bcb82008d8a635d161bf1480773897d2ed12
Liz <ecmessina@gmail.com>
2024-07-24 12:15:21 -0400

    Created activity model mapping the API response

---
+++ lib/activity_model.dart
@@ -0,0 +1,25 @@
+class ActivityModel {
+  final String activity;
+  final double availability;
+  final String type;
+  final int participants;
+  final double price;
+  final String accessibility;
+  final String duration;
+  final bool kidFriendly;
+  final String link;
+  final String key;
+
+  ActivityModel({
+    required this.activity,
+    required this.availability,
+    required this.type,
+    required this.participants,
+    required this.price,
+    required this.accessibility,
+    required this.duration,
+    required this.kidFriendly,
+    required this.link,
+    required this.key,
+  });
+}
