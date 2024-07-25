a38c22ea153ae811df3a5a9a388009bfeadf66d5
Liz <ecmessina@gmail.com>
2024-07-25 19:45:20 +0000

    Merged in IB-4 (pull request #2)
    Added Riverpod
    
    * Added Riverpod and test FutureProvider
    
    
    Approved-by: David Messina

---
+++ lib/providers/random_activity_provider.dart
@@ -0,0 +1,8 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'random_activity_provider.g.dart';
+
+@riverpod
+Future<String> randomActivity(RandomActivityRef ref) async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 'Got the data!';
+}
---
+++ lib/providers/random_activity_provider.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'random_activity_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$randomActivityHash() => r'80527c0ca831e1cebcc2b97e8a7e47909ef01f93';
+
+/// See also [randomActivity].
+@ProviderFor(randomActivity)
+final randomActivityProvider = AutoDisposeFutureProvider<String>.internal(
+  randomActivity,
+  name: r'randomActivityProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product') ? null : _$randomActivityHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RandomActivityRef = AutoDisposeFutureProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
