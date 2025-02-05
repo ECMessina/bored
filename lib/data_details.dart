ef9cf0ea74136c3ba0d15a5c60fb778183e00be7
Liz <ecmessina@gmail.com>
2025-02-05 12:24:17 -0500

    Added link animation

--- lib/data_details.dart
+++ lib/data_details.dart
@@ -3,8 +3,9 @@ import 'package:bored/constants.dart';
 import 'package:bored/models/activity_model.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:validators/validators.dart';
 
-class DataDetails extends StatelessWidget {
+class DataDetails extends StatefulWidget {
   const DataDetails({
     super.key,
     required this.activityModel,
@@ -17,6 +18,32 @@ class DataDetails extends StatelessWidget {
   final Function() onPressed;
 
   @override
+  State<DataDetails> createState() => _DataDetailsState();
+}
+
+class _DataDetailsState extends State<DataDetails>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    )..repeat();
+    _animation =
+        Tween<double>(begin: 0.0, end: 2.0).animate(_animationController);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
@@ -25,7 +52,7 @@ class DataDetails extends StatelessWidget {
         children: [
           const Spacer(),
           Text(
-            '${activityModel.activity}!',
+            '${widget.activityModel.activity}!',
             style: boldActivityTextStyle,
             textAlign: TextAlign.center,
           ),
@@ -44,16 +71,18 @@ class DataDetails extends StatelessWidget {
               spacing: 10,
               children: [
                 Text(
-                  activityModel.getActivityTypeText(),
+                  widget.activityModel.getActivityTypeText(),
                   style: activityTextStyle,
                   textAlign: TextAlign.center,
                 ),
                 Text(
-                  activityModel.participants == 1 ? 'Only requires 1 person' : 'Requires ${activityModel.participants} people',
+                  widget.activityModel.participants == 1
+                      ? 'Only requires 1 person'
+                      : 'Requires ${widget.activityModel.participants} people',
                   style: activityTextStyle,
                 ),
                 Text(
-                  activityModel.accessibility,
+                  widget.activityModel.accessibility.toString(),
                   style: activityTextStyle,
                 ),
                 Text(
@@ -71,17 +100,17 @@ class DataDetails extends StatelessWidget {
                 ),
                 ),
                 Text(
-                  activityModel.getPriceForActivity(),
+                  widget.activityModel.getPriceForActivity(),
                   style: activityTextStyle,
                 ),
               ],
             ),
           ),
           const Spacer(),
-          if (activityModel.link.isNotEmpty)
+          if (isURL(widget.activityModel.link))
             GestureDetector(
               onTap: () async {
-                final uri = Uri.parse(activityModel.link);
+                final uri = Uri.parse(widget.activityModel.link);
                 if (!await launchUrl(uri)) {
                   if (context.mounted) {
                     ScaffoldMessenger.of(context).showSnackBar(
@@ -95,17 +124,48 @@ class DataDetails extends StatelessWidget {
                   }
                 }
               },
-              child: Text(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnimatedBuilder(
+                    animation: _animationController,
+                    builder: (context, child) {
+                      return AnimatedRotation(
+                        turns: 0.25 - (_animation.value * 0.25),
+                        duration: Duration(seconds: 2),
+                        child: Icon(
+                          Icons.arrow_forward,
+                          color: Colors.purple.shade600,
+                        ),
+                      );
+                    },
+                  ),
+                  Text(
                 'Learn More!',
                 style: linkTextStyle,
                 textAlign: TextAlign.center,
+              ),
+                  AnimatedBuilder(
+                    animation: _animationController,
+                    builder: (context, child) {
+                      return AnimatedRotation(
+                        turns: -0.25 - (_animation.value * 0.25),
+                        duration: Duration(seconds: 2),
+                        child: Icon(
+                          Icons.arrow_forward,
+                          color: Colors.purple.shade600,
+                        ),
+                      );
+                    },
+                  ),
+                ],
               ),
             ),
           const Spacer(),
           ToDoButton(
             icon: Icons.redo,
-            label: label,
-            onPressed: onPressed,
+            label: widget.label,
+            onPressed: widget.onPressed,
           ),
           const Spacer(),
         ],
